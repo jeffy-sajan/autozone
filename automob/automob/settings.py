89951fd500e710c7da4f4600de4012cc2543ec49
Jeffy Sajan <149129289+jeffy-sajan@users.noreply.github.com>
2024-10-07 22:23:30 +0530

    Update settings.py

--- automob/automob/settings.py
+++ automob/automob/settings.py
@@ -76,9 +76,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'automob.wsgi.application'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -125,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-media_url='/media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
